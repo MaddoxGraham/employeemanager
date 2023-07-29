0626f3e2fe6c44803b002affe79a4fd04077977e
MaddoxGraham <d.raimbault29@gmail.com>
2023-07-29 13:46:01 +0200

    FrontEnd_build28/07/23

--- employeemanagerapp/src/app/app.component.ts
+++ employeemanagerapp/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -26,6 +27,33 @@ export class AppComponent implements OnInit{
         alert(error.message);
       }
     )
+  }
+
+  public onAddEmployee(addForm: NgForm): void {
+
+  }
+
+  // on va créer une fonction qui va générer les modales en fonction du bouton mode sera le boutton sur lequel on as cliqué
+  public onOpenModal( employee: Employee | null,mode: string): void{
+    const container = document.getElementById('main');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-bs-toggle','modal');
+
+    if (mode === 'add'){
+      button.setAttribute('data-bs-target','#addEmployee');
+    }
+    if( mode === 'edit'){
+      button.setAttribute('data-bs-target','#updateEmployee');
+    }
+    if (mode === 'delete'){
+      button.setAttribute('data-bs-target','#deleteEmployee');
+    }
+
+    container?.appendChild(button);
+    button.click();
+
   }
 
 }
